50ae7cdb3d1b35859ae4b2c4f48c8e85c8446654
Daniel Plominski <Daniel@Plominski.eu>
2015-04-15 17:25:10 +0200

    prototype 0.2

--- lxc-to-go.sh
+++ lxc-to-go.sh
@@ -829,10 +829,7 @@ fi
 
 
 ### ### ### ### ### ### ### ### ###
-#/ bash -c echo -e '\E[33;44m'"bootstrap finished"; tput sgr0
-#/ echo " `Control V``Control [`[33m bootstrap finished"
-#/ echo "^[[0;31;40m bootstrap finished"
-#/ echo -e "\e[1;31m bootstrap finished \e[0m"
+echo "" # printf
 printf "\033[1;31mbootstrap finished\033[0m\n"
 ### ### ### ### ### ### ### ### ###
 #
