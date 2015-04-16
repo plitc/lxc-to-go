0660e8e30485a03994eaab2313d2e43ee4862dfc
Daniel Plominski <Daniel@Plominski.eu>
2015-04-16 20:14:45 +0200

    prototype 0.8.3

--- lxc-to-go.sh
+++ lxc-to-go.sh
@@ -254,8 +254,8 @@ GETIPV4DEFAULTGATEWAYVALUE=$(grep -s "IPV4DEFAULTGATEWAY" /tmp/lxc-to-go_IPV4GAT
       GETIPV4UDEV=$(ifconfig "$GETBRIDGEPORT0" | grep "inet " | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1)
       GETIPV4SUBNETUDEV=$(ifconfig "$GETBRIDGEPORT0" | grep "inet " | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1 | sed 's/255.255.255.0/24/' | sed 's/255.255.255.224/27/')
       ifconfig vswitch0 inet "$GETIPV4UDEV"/"$GETIPV4SUBNETUDEV"
-###/ ip addr flush eth0
-###/ ip addr del "$GETIPV4UDEV"/"$GETIPV4SUBNETUDEV" dev "$GETBRIDGEPORT0"
+###/ip addr flush eth0
+      ip addr del "$GETIPV4UDEV"/"$GETIPV4SUBNETUDEV" dev "$GETBRIDGEPORT0"
       route del default
       route add default gw "$GETIPV4DEFAULTGATEWAYVALUE" dev vswitch0
 ### fix //
@@ -268,8 +268,8 @@ fi
       GETIPV4=$(ifconfig eth0 | grep "inet " | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -n 1)
       GETIPV4SUBNET=$(ifconfig eth0 | grep "inet " | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1 | sed 's/255.255.255.0/24/' | sed 's/255.255.255.224/27/')
       ifconfig vswitch0 inet "$GETIPV4"/"$GETIPV4SUBNET"
-###/ ip addr flush eth0
-###/ ip addr del "$GETIPV4"/"$GETIPV4SUBNET" dev eth0
+###/ip addr flush eth0
+      ip addr del "$GETIPV4"/"$GETIPV4SUBNET" dev eth0
       route del default
       route add default gw "$GETIPV4DEFAULTGATEWAYVALUE" dev vswitch0
 ### fix //
