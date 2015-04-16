aee7a2dd0e31c7db31f75d3da843aeef0134a377
Daniel Plominski <Daniel@Plominski.eu>
2015-04-16 18:37:27 +0200

    prototype 0.8

--- hook_deb7.sh
+++ hook_deb7.sh
@@ -2,7 +2,13 @@
 ### ### ### PLITC ### ### ###
 echo "<--- --- --- debian 7 lxc template hooks // --- --- --->"
 
+run(){
+   # execute inside lxc
+   run1=$1
+   lxc-attach -n deb7template -- $1
+}
 
+#/ run echo ""
 
 echo "<--- --- --- // debian 7 lxc template hooks --- --- --->"
 #/ exit 0
--- hook_deb8.sh
+++ hook_deb8.sh
@@ -2,7 +2,13 @@
 ### ### ### PLITC ### ### ###
 echo "<--- --- --- debian 8 lxc template hooks // --- --- --->"
 
+run(){
+   # execute inside lxc
+   run1=$1
+   lxc-attach -n deb8template -- $1
+}
 
+#/ run echo ""
 
 echo "<--- --- --- // debian 8 lxc template hooks --- --- --->"
 #/ exit 0
