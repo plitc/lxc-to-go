acb2b17c619a865f3934e0b98ca0319fd79c4352
Daniel Plominski <Daniel@Plominski.eu>
2015-04-22 05:54:22 +0200

    prototype 0.14.4

--- lxc-to-go.sh
+++ lxc-to-go.sh
@@ -1645,6 +1645,16 @@ else
    ### ### ### ### ### ### ### ### ###
 fi
 
+PS3='Choose one word: ' 
+
+# bash select
+select word in "linux" "bash" "scripting" "tutorial" 
+do
+  echo "The word you have selected is: $word"
+# Break, otherwise endless loop
+  break  
+done
+
 ### ### ### ### ### ### ### ### ###
 #
 ### // stage4 ###
