dff46c8db474f1a4ae62fe8e8ffd0422c806bfbf
Daniel Plominski <Daniel@Plominski.eu>
2015-04-23 13:15:41 +0200

    prototype 0.20.1

--- modification/wlan_support.sh
+++ modification/wlan_support.sh
@@ -2,10 +2,18 @@
 ### ### ### lxc-to-go // ### ### ###
 echo "<--- --- --- lxc-to-go (server variant) wlan support  // --- --- --->"
 
+cp -prf ../lxc-to-go.sh lxc-to-go_wlan.sh
 
+### MOD // ###
+#
+sed -i "s/eth0/wlan0/g" lxc-to-go_wlan.sh
+#
+### // MOD ###
+
+cp -prf lxc-to-go_wlan.sh ../lxc-to-go_wlan.sh
+rm -f lxc-to-go_wlan.sh
 
 echo "<--- --- --- // lxc-to-go (server variant) wlan support  --- --- --->"
-
 exit 0
 ### ### ### // lxc-to-go ### ### ###
 # EOF
