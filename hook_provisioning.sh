fdf1998ba0e50e6282be979256421e10cb6f1308
Daniel Plominski <Daniel@Plominski.eu>
2015-04-23 02:04:39 +0200

    prototype 0.17.3

---
+++ hook_provisioning.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+### ### ### lxc-to-go // ### ### ###
+echo "<--- --- --- provisioning hooks // --- --- --->"
+
+run(){
+   # execute commands inside the lxc template
+   lxc-attach -n "$LXCCREATENAME" -- $*
+}
+
+### EXAMPLE // ###
+#
+
+run echo example
+
+#
+### // EXAMPLE ###
+
+echo "<--- --- --- // provisioning hooks --- --- --->"
+#/ exit 0
+### ### ### // lxc-to-go ### ### ###
+# EOF
