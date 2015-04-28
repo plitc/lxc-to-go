29500141ea196c81091e231a3a1c74c3f53dce73
Daniel Plominski <Daniel@Plominski.eu>
2015-04-28 13:21:49 +0200

    prototype 0.25.6

---
+++ lxc-to-go-template.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+### LICENSE - (BSD 2-Clause) // ###
+#
+# Copyright (c) 2015, Daniel Plominski (Plominski IT Consulting)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+### // LICENSE - (BSD 2-Clause) ###
+
+### ### ### PLITC // ### ### ###
+
+### stage0 // ###
+DEBIAN=$(grep -s "ID" /etc/os-release | egrep -v "VERSION" | sed 's/ID=//g')
+DEBVERSION=$(grep -s "VERSION_ID" /etc/os-release | sed 's/VERSION_ID=//g' | sed 's/"//g')
+MYNAME=$(whoami)
+###
+#/ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PRG="$0"
+# need this for relative symlinks
+while [ -h "$PRG" ] ;
+   do
+   PRG=`readlink "$PRG"`
+   done
+DIR=`dirname "$PRG"`
+###
+### // stage0 ###
+
+### stage1 // ###
+if [ "$DEBIAN" = "debian" ]; then
+   : # dummy
+else
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+fi
+### stage2 // ###
+
+### // stage2 ###
+#
+### stage3 // ###
+if [ "$MYNAME" = "root" ]; then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+if [ "$DEBVERSION" = "7" ]; then
+   : # dummy
+else
+   if [ "$DEBVERSION" = "8" ]; then
+   : # dummy
+   else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You need Debian 7 (Wheezy) or 8 (Jessie) Version"
+   exit 1
+   fi
+fi
+CHECKLXCINSTALL=$(/usr/bin/which lxc-checkconfig)
+if [ -z "$CHECKLXCINSTALL" ]; then
+   echo "" # dummy
+   printf "\033[1;31mLXC 'managed' doesn't run, execute the 'bootstrap' command at first\033[0m\n"
+   exit 1
+fi
+if [ -z "$DIALOG" ]; then
+   echo "<--- --- --->"
+   echo "need dialog"
+   echo "<--- --- --->"
+   apt-get update
+   apt-get -y install dialog
+   echo "<--- --- --->"
+fi
+#
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+CHECKBRIDGE1=$(ifconfig | grep -c "vswitch0")
+if [ "$CHECKBRIDGE1" = "0" ]; then
+   ### ### ### ### ### ### ### ### ###
+   echo "" # printf
+   printf "\033[1;31mCan't find the Bridge Zones, execute the 'bootstrap' command at first\033[0m\n"
+   exit 1
+   ### ### ### ### ### ### ### ### ###
+fi
+
+CHECKLXCCONTAINER=$(lxc-ls | egrep -c "managed|deb7template|deb8template")
+if [ "$CHECKLXCCONTAINER" = "3" ]; then
+   : # dummy
+else
+   ### ### ### ### ### ### ### ### ###
+   echo "" # printf
+   printf "\033[1;31mCan't find all nessessary default lxc container, delete the 'managed|deb7template|deb8template' lxc and execute the 'bootstrap' command again\033[0m\n"
+   exit 1
+   ### ### ### ### ### ### ### ### ###
+fi
+
+### TEMPLATE // ###
+
+### // TEMPLATE ###
+
+### ### ###
+echo ""
+printf "\033[1;31mlxc-to-go template selection finished.\033[0m\n"
+### ### ###
+
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+exit 0
+### ### ### PLITC ### ### ###
+# EOF
