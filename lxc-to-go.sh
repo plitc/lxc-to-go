e99dbbb1e4b99603e78e72163b71633329778b92
Daniel Plominski <Daniel@Plominski.eu>
2015-05-09 05:38:05 +0200

    prototype 0.32.2

--- lxc-to-go.sh
+++ lxc-to-go.sh
@@ -1919,10 +1919,7 @@ echo "FOUND:"
 lxc-ls | egrep -v "managed|deb7template|deb8template" | tr '\n' ' '
 echo "" # dummy
 
-#/ lxc-ls | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I {} screen -d -m -S "{}" -- lxc-start -n "{}"
-#/ lxc-ls | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I % sh -c '{ screen -d -m -S "%" -- lxc-start -n "%"; sleep 5; }'
-#/ lxc-ls | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I % sh -c '(screen -d -m -S "%" -- lxc-start -n "%";sleep 10;) & spinner $!'
-lxc-ls | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I % sh -c '{ screen -d -m -S "%" -- lxc-start -n "%"; sleep 10; }' & spinner $!
+lxc-ls | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I % sh -c '{ screen -d -m -S "%" -- lxc-start -n "%"; sleep 5; }' & spinner $!
 
 echo "" # dummy
 echo "... LXC Container (screen sessions): ..."
@@ -2074,7 +2071,7 @@ if [ -e "$CHECKFORWARDINGFILE" ]; then
 fi
 ### // FORWARDING ###
 
-lxc-ls --active | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I % sh -c '{ lxc-stop -n "%"; sleep 5; }'
+lxc-ls --active | egrep -v "managed|deb7template|deb8template" | xargs -L1 -I % sh -c '{ lxc-stop -n "%"; sleep 5; }' & spinner $!
 
 ### ### ###
 echo "" # printf
