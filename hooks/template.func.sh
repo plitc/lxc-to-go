345ee2815a24b4c72f0a5326aa616e76b2af6a0a
Daniel Plominski <daniel@notebook1-plitc.global.plitc.eu>
2015-12-12 15:35:47 +0100

    Version: 0.40.7.5

--- hooks/template.func.sh
+++ hooks/template.func.sh
@@ -3,12 +3,12 @@
 #// version: 1.0
 
 RUN(){
-   # execute commands inside the lxc template
+   #// execute commands inside the lxc template
    lxc-attach -n "$LXCCREATENAME" -- "$@"
 }
 
 CHECK(){
-   # check state
+   #// check state
    if [ $? -eq 0 ]
    then
       echo "[$(printf "\033[1;32m  OK  \033[0m\n")] '"$@"'"
@@ -20,11 +20,26 @@ CHECK(){
 }
 
 LOL(){
-   # break
+   #// break
    sleep 2
    echo ""
    echo "---> next step: "$@" <---"
    echo ""
+}
+
+SPINNER(){
+   #// spinner
+   local pid=$1
+   local delay=0.01
+   local spinstr='|/-\'
+   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+         local temp=${spinstr#?}
+         printf " [%c]  " "$spinstr"
+         local spinstr=$temp${spinstr%"$temp"}
+         sleep $delay
+         printf "\b\b\b\b\b\b"
+   done
+   printf "    \b\b\b\b"
 }
 
 ### ### ### // lxc-to-go ### ### ###
